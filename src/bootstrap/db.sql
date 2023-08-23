ab73f9c12b518289990901627b351e8192d0c224
bertjanstroop-prive <65427840+bertjanstroop-prive@users.noreply.github.com>
2023-08-23 21:18:08 +0200

    Create db.sql

---
+++ src/bootstrap/db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE software;
+USE DATABASE software;
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
